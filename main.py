80ce0f94dfb7f75f0e699aa015370b84ff6b0c7d
AngeloDima <angelodimauro91@gmail.com>
2023-09-01 23:45:44 +0200

    valori booleani

--- main.py
+++ main.py
@@ -1,57 +1,34 @@
-# stringa normale
-x = "ciao"
+#Booleani
+q = True
+w = False
+
+if 5 < 10:
+    print("ciao sono minore di 10")
+else:
+    print("ciao sono maggiore di 10")
+
+# valutare il valore
+r = 0
+t = 1
+
+print(bool(t))
+
+pane = 0
+u = 1
+
+if pane:
+    print("non serve andare al supermercato")
+else:
+    print("uscire a prendere il pane")
 
 
-# Stringe multi riga
-y = """ciao
-        come
-        va 
-        io
-        tutto
-        bene"""
+#-----
 
-# prendimi l'indice della stringa x
-print(len(x))
+i = []
 
+if i:
+    print("comprare cose")
+else:
+    print("ho tutto")
 
-# ciclo for
-for carattere in "computer":
-    print(carattere)
-
-# stampa fino al carattere ...
-a = "ciao sono Angelo"
-print(a[:4])
-
-# stampa dal carattere ...
-b = "ciao sono Angelo"
-print(a[4:])
-
-# stampa dentro quel range ...
-o = "ciao sono Angelo"
-print(a[4:8])
-
-
-# indice negativo leva le ultime lettere 
-u = "ciao python"
-print(u[-2:])
-
-
-# modificare una stringa
-k = " ciao sono angelo "
-print(k.upper())  #TUTTO GRANDE
-print(k.lower())  #TUTTO PICCOLO
-print(k.strip())  #LEVA LO SPAZZIO START - END
-print(k.replace("o", "K"))  #RIMPIAZZA
-
-
-
-#concatenare Stringe e numeri
-c = 20
-m = "ciao sono angelo e ho {} anni nato il {}"  #se specifici l'indice verrano ordinati come vuoi tu
-print (m.format(c, 2002)) #se metti più valori verrano messi in ordine di scrittura
-
-
-
-#Escape dei caratteri
-h = "ciao sono angelo e voglio un \"gelato\""
-print(h)
+print(bool(i))
