10912c916ddc535f1967fb504c06c2bd9a404a24
AngeloDima <angelodimauro91@gmail.com>
2023-09-01 23:06:23 +0200

    Cast dei dati

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
+x = int(10.93)
+y = str(2)
+k = int("1")
+l = float(12)
 
-str 
-x = "ciao"
-int
-c = 10
-float
-v = 10.10
-bool
-a = True
-list
-q = ["ciao", "ola", "cacca"]
-tuple
-p = ("ciao", "ola", "cacca")
-range
-i = range(10)
-dict
-j = {"nome" : "Angelo", "nome" : "Olaf"}
-set
-g = {"Roma", "catania"}
+print(l)
