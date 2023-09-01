3a0f51d74abfc386fa7688677c34e5c2f988d9a6
AngeloDima <angelodimauro91@gmail.com>
2023-09-01 22:58:39 +0200

    istallo python e prendo i vari "elementi"

---
+++ main.py
@@ -0,0 +1,19 @@
+
+str 
+x = "ciao"
+int
+c = 10
+float
+v = 10.10
+bool
+a = True
+list
+q = ["ciao", "ola", "cacca"]
+tuple
+p = ("ciao", "ola", "cacca")
+range
+i = range(10)
+dict
+j = {"nome" : "Angelo", "nome" : "Olaf"}
+set
+g = {"Roma", "catania"}
