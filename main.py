f2eabee5c5ac68b5366642efdaface488c1d97a7
AngeloDima <angelodimauro91@gmail.com>
2023-09-01 23:30:27 +0200

    metodi per manipolare le stringhe

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
-x = int(10.93)
-y = str(2)
-k = int("1")
-l = float(12)
+# stringa normale
+x = "ciao"
 
-print(l)
+
+# Stringe multi riga
+y = """ciao
+        come
+        va 
+        io
+        tutto
+        bene"""
+
+# prendimi l'indice della stringa x
+print(len(x))
+
+
+# ciclo for
+for carattere in "computer":
+    print(carattere)
+
+# stampa fino al carattere ...
+a = "ciao sono Angelo"
+print(a[:4])
+
+# stampa dal carattere ...
+b = "ciao sono Angelo"
+print(a[4:])
+
+# stampa dentro quel range ...
+o = "ciao sono Angelo"
+print(a[4:8])
+
+
+# indice negativo leva le ultime lettere 
+u = "ciao python"
+print(u[-2:])
+
+
+# modificare una stringa
+k = " ciao sono angelo "
+print(k.upper())  #TUTTO GRANDE
+print(k.lower())  #TUTTO PICCOLO
+print(k.strip())  #LEVA LO SPAZZIO START - END
+print(k.replace("o", "K"))  #RIMPIAZZA
+
+
+
+#concatenare Stringe e numeri
+c = 20
+m = "ciao sono angelo e ho {} anni nato il {}"  #se specifici l'indice verrano ordinati come vuoi tu
+print (m.format(c, 2002)) #se metti più valori verrano messi in ordine di scrittura
+
+
+
+#Escape dei caratteri
+h = "ciao sono angelo e voglio un \"gelato\""
+print(h)
