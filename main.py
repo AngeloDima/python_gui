31ec3032749324255056de86cdfb897e65ecaf64
AngeloDima <angelodimauro91@gmail.com>
2023-09-02 02:34:41 +0200

    tuple

--- main.py
+++ main.py
@@ -1,100 +1,46 @@
-# List
+#tuple
 
-x = ["Milano", "Roma", "Napoli", "Augusta"]
-y = ["ciao", 1231, True]
-z = list(("cane", "gatto", "elefante"))
+q  = tuple(("Milano", "Catania"))
+w = ("Catania", True, 4)
 
-# len(), type(), list()
-print(len(x))
-print(type(x))
-print(x)
-print(z)
+print((q))
 
-#lavorare con i range
-k = ["fragola", "carota", "mandarino", "elefante"]
-k[1:3] = ["Roma", "Inter"]
+#Controllo se un elemento esiste dentro una tuple o lista
+if "Catania" in q:
+    print("OK")
+else:
+    print("non trovato")
 
-print(k)
-
-
-#append     SI AGGIUNGE ALLA FINE
-mm = ["ciao", "lollo"] 
-mm.append("ola")
-
-print(mm)
+print("------")
+print("------")
+print("------")
+print("------")
 
 
-#insert     SI INSERISCE NEL INDICE SPECIFICATO E SPOSTA TUTTO IL RESTO
-oi = ["prova", "della", "asd"]
-oi.insert(1, "ciao")
-print(oi)
+#manipolare i diversi "contenitori" e trasformali a piacimento ESS...
+#Le tuple non possono essere modificate le liste si
+e = ("India", "Monaco", "Palermo")
+r = list(e)
+r[0] = "italia"
+e = tuple(r)
+print(r)
 
 
-#extend     E COME UN APPEND MA CON PIU DATI QUINDI CON DEGLI ARRAY(List)
-kj = ["bergamo", "bologna", "Taormina"]
-ca = ["Glovo", "deliveroo"]
-kj.extend(ca)
-print(kj)
+print("------")
+print("------")
+print("------")
+print("------")
 
+#spappolare una tupla in tante var
+container = ("strappo", "slancio", "stacco", "panca", "panca")
+(i,o,*p) = container
+print(i)
+print(o)
+print(p)
 
+#metodi
+l = container.count("panca")
+print(l)
 
-#REMOVE  SPECIFICARE COSA VUOI ELIMINARE
-jj = ["ciao", "come", "alla"]
-jj.remove("ciao")
-print(jj)
-
-
-#POP  ELIMINA L'ULTIMO O SPECIFICA L'INDICI
-jj = ["ciao", "come", "alla"]
-jj.pop()
-print(jj)
-
-
-#DEL   NON HO CAPITO
-jj = ["ciao", "come", "alla"]
-del jj[1]
-print(jj)
-
-
-#CLEAR   CANCELLA TUTTO
-jj = ["ciao", "come", "alla"]
-jj.clear()
-print(jj)
-
-
-
-
-#CICLARE LOOP
-jj = ["ciao", "come", "alla"]
-for parola in jj:
-    print(parola)
-
-
-
-#con il range 
-for i in range(len(jj)):
-    print(jj[i])
-
-#con il while
-po = ["q", "w"]
-i = 0
-while i < len(po):
-    print(po[i])
-    i += 1
-
-
-#unire insieme più liste
-d = ["Genova", "Gela"]
-r = ["Italia", "America"]
-print(d + r)
-
-#con il for
-for info in r:
-    d.append(info)
-print(d)
-
-#con extend
-d.extend(r)
-print(d)
-
-
+l = container.index("panca")
+print(l)
