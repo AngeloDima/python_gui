b67afd10f24ce7a227f4d7143ef081db007ad5ac
AngeloDima <angelodimauro91@gmail.com>
2023-09-03 00:37:49 +0200

    user input

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
-#Try Except
+#User input
+import sys
+persona = {
+    "nome": "Luca",
+    "cognome": "Rossi",
+    "eta": 21
+}
 
-x = 5
+operazioni = ("aggiungere", "modificare", "eliminare", "stoppa")
 
-try:
-    print(x)
-except NameError:
-    print("x non definita")
-except TypeError:                   #Intervieni sui problemi puoi specificare e non l'errore
-    print("numero e stringa")
-else:                               #Se tutto va bene
-    print("nessun problema")
+def start():
+    operazione = input("cosa vuoi fare? ")
+    if operazione == operazioni[0]:
+        param = input("Aggiungi chiave valore: separati da una virgola ")
+        aggiungi(param.split(","))
+
+    elif operazione == operazioni[1]:
+        chiave = input("Inserisci la chiave da modificare: ")
+        valore = input("Inserisci il valore da modificare: ")
+        modifica(chiave, valore)
+
+    elif operazione == operazioni[2]:
+        chiave = input("inserisci la chiave della persona da eliminare: ")
+        eliminare(chiave)
+    
+
+def aggiungi(param):
+    chiave = param[0]
+    valore = param[1]
+    persona[chiave]=valore
+    print(persona)
 
 
-#Creiamo noi un errore
-i = -1
-if i < 0:
-    raise Exception("numero minore di zero")
-print("ciao")
+def modifica(chiave, valore):
+    if chiave in persona:
+        persona[chiave] = valore
+        print(persona)
+    else:
+        print("chiave non trovata nella persona")
+
+
+def eliminare(chiave):
+    if chiave in persona:
+        del persona[chiave]
+        print(f"Chiave '{chiave}' eliminata dalla persona")
+    else:
+        print("Chiave non trovata nella persona")
+
+
+
+
+while True:
+    start()
+
+
