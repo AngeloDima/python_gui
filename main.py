8647901387c5a2b8995ceca11fb6163411f69822
AngeloDima <angelodimauro91@gmail.com>
2023-09-02 14:48:12 +0200

    Dict

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-#set
-x = {"milano", "roma", "napoli"}
-y = {"venezia", "udine"}
+#dictionary
 
-# possiamo solo aggiungere o rimuovere elementi
-x.add("venezia")
+    # get(), keys(), values(), item()
 
-x.update(y)
-x.remove("milano")
-x.discard("cagliari")
+persona = {
+    "nome": "angelo",
+    "cognome": "di mauro",
+    "eta": 20,
+    #si possono avere dict dentro i dict come se fosse un normale JSON
+    "indirizzo": {
+        "città": "augusta"
+    }
+}
+print(persona["indirizzo"]["città"])
 
+
+#modificare elementi con [] e update
+persona["nome"] = "Marco"
+persona.update({"nome": "anna"})
+
+#aggiungere elementi con [] e update
+persona["colore"] = "verde"
+persona.update({"colore": "giallo"})
+
+#rimuovere elementi con pop(), popitem(), clear(), del()
+
+persona.pop("nome")
+persona.clear()
+del persona
+
+
+#ciclare
+for x in persona:   #oppure mettere dopo persona .values(), keys() ecc quello che vogliamo prendere
+    print(persona[x])
+
+
+
+print(persona)
+
+
+#copiare   copy()
+
+x = persona.copy()
 print(x)
 
-
-
-#possiamo unire
-u = {"italia", "america", "trento"}
-l = {"palermo", "catania", "siracusa", "america"}
-
-z = u.union(l)  #genera un nuovo set
-u.update(l)     #aggiorna il set
-
-#intersection
-u.intersection_update(l)
-
-print(u)
