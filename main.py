2e07605856818d1634978e672d5db7150c170918
AngeloDima <angelodimauro91@gmail.com>
2023-09-02 17:36:11 +0200

    math

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-#dateTime
+# Math
+#min max abs pow
 
-# %d: Giorno del mese (01-31)
-# %m: Mese (01-12)
-# %Y: Anno (es. 2023)
-# %H: Ore (00-23)
-# %M: Minuti (00-59)
-# %S: Secondi (00-59)
-# %A: Nome del giorno (es. Lunedì)
-# %B: Nome del mese (es. Gennaio)
-# %Y-%m-%d %H:%M:%S: Formato esteso con data e orario
 
-import datetime
+x = 3 + 5
+y = x + 10
+z = ((x + y) * 2 + 35) / x
 
-x = datetime.datetime.now()
-print(x.strftime("%d/ %m/ %Y"))
+
+
+a = min(4,1,2)   #darà il numero più piccolo
+s = max(4,1,2)   #darà il numero più grande
+q = pow(4, 3)    #darà la potenza
+
+#modulo importato
+import math
+
+o = math.sqrt(64)  #radice ma serve il modulo Math
+print(o)
+
+
+v = math.ceil(10.8)    #Eccesso
+print(v)
+
+g = math.floor(10.8)   #Difetto
+print(g)
+
+
+c = math.pi      #pigreco
+print(c)
