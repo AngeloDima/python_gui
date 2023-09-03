a390ad9c44d813aa4aa9e0c99d017a8c922cb66f
AngeloDima <angelodimauro91@gmail.com>
2023-09-03 02:06:53 +0200

    creare un db e una table

--- main.py
+++ main.py
@@ -2,3 +2,35 @@
 # pip install-connector-python
 
 import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="pysql"
+)
+
+# cursor = db.cursor()
+
+# CREATE DATABASE
+# SHOW DATABASES
+# cursor.execute("SHOW DATABASES")
+
+# for x in cursor:
+#     print(x)
+
+
+#CREARE UNA TABLE
+# cursor = db.cursor()
+# cursor.execute("CREATE TABLE clienti (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHARD (255))")
+
+
+
+#VEDERE LE TABLE
+# cursor = db.cursor()
+# cursor.execute("SHOW TABLES")
+# for x in cursor:
+#     print(x)
+
+
+
